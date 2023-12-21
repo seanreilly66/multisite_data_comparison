bc98c4c43aaf3962f827ca8c0ed40ad32352fe2e
Sean Reilly <sean.reilly66@gmail.com>
2023-12-20 18:15:22 -0800

    Expand output metrics

--- R/raster_spec_metric_extraction.R
+++ R/raster_spec_metric_extraction.R
@@ -39,17 +39,19 @@ library(sf)
 library(tidyverse)
 library(doParallel)
 library(glue)
+library(Hmisc)
+library(moments)
 
 # ================================= User inputs ================================
 
 gdb <- 'data/ssu_3dforests.gdb'
 plot_shp_file <- 'field_plots'
 
-rast_folder <- 'data/planet_imagery/site_raster'
-raw_folder <- 'data/planet_imagery/raw_imagery'
+raster_folder <- 'data/planet_imagery/raw_imagery'
 file_pattern <- 'SR.tif$'
 
-output_csv <- 'data/predictor_df/planet_spec_predictors'
+raw_val_csv <- 'data/spectral/planet_spec_val.csv'
+metric_output_csv <- 'data/predictor_df/planet_spec_predictors.csv'
 
 n_cluster = 8
 
@@ -63,7 +65,7 @@ record_id <- plot_shp %>%
   select(ID, campaign, plot) %>%
   st_drop_geometry()
   
-rast_files <- list.files(raw_folder, pattern = file_pattern, full.names = T)
+rast_files <- list.files(raster_folder, pattern = file_pattern, full.names = T)
 
 cl <- makeCluster(n_cluster)
 registerDoParallel(cl)
@@ -77,6 +79,7 @@ rast_val <- foreach(
   rast_i <- rast(file_i)
   
   rast_i$ndvi <- (rast_i$nir - rast_i$red) / (rast_i$nir + rast_i$red)
+  rast_i$gndvi <- (rast_i$nir - rast_i$green) / (rast_i$nir + rast_i$green)
   
   rast_val = terra::extract(rast_i, plot_shp, exact = T) %>%
     filter(!is.na(ndvi)) %>%
@@ -84,21 +87,55 @@ rast_val <- foreach(
   
 }
 
+stopCluster(cl)
+
 rast_metrics <- rast_val %>%
   group_by(campaign, plot) %>%
-  summarize(
-    blue_mean = mean(blue),
-    green_mean = mean(green),
-    red_mean = mean(red),
-    nir_mean = mean(nir),
-    ndvi_mean = mean(ndvi),
-    blue_sd = sd(blue),
-    green_sd = sd(green),
-    red_sd = sd(red),
-    nir_sd = sd(nir),
-    ndvi_sd = sd(ndvi)
-    
+  dplyr::summarize(
+    blue_mean = wtd.mean(blue, fraction),
+    green_mean = wtd.mean(green, fraction),
+    red_mean = wtd.mean(red, fraction),
+    nir_mean = wtd.mean(nir, fraction),
+    ndvi_mean = wtd.mean(ndvi, fraction),
+    gndvi_mean = wtd.mean(gndvi, fraction),
+    blue_sd = sqrt(wtd.var(blue, fraction)),
+    green_sd = sqrt(wtd.var(green, fraction)),
+    red_sd = sqrt(wtd.var(red, fraction)),
+    nir_sd = sqrt(wtd.var(nir, fraction)),
+    ndvi_sd = sqrt(wtd.var(ndvi, fraction)),
+    gndvi_sd = sqrt(wtd.var(gndvi, fraction)),
+    blue_iqr = IQR(blue),
+    green_iqr = IQR(green),
+    red_iqr = IQR(red),
+    nir_iqr = IQR(nir),
+    ndvi_iqr = IQR(ndvi),
+    gndvi_iqr = IQR(gndvi),
+    blue_max = max(blue),
+    green_max = max(green),
+    red_max = max(red),
+    nir_max = max(nir),
+    ndvi_max = max(ndvi),
+    gndvi_max = max(gndvi),
+    blue_min = min(blue),
+    green_min = min(green),
+    red_min = min(red),
+    nir_min = min(nir),
+    ndvi_min = min(ndvi),
+    gndvi_min = min(gndvi),
+    blue_skew = skewness(blue),
+    green_skew = skewness(green),
+    red_skew = skewness(red),
+    nir_skew = skewness(nir),
+    ndvi_skew = skewness(ndvi),
+    gndvi_skew = skewness(gndvi),
+    blue_kurt = kurtosis(blue),
+    green_kurt = kurtosis(green),
+    red_kurt = kurtosis(red),
+    nir_kurt = kurtosis(nir),
+    ndvi_kurt = kurtosis(ndvi),
+    gndvi_kurt = kurtosis(gndvi)
   )
 
-write_csv(rast_metrics, output_csv)
 
+write_csv(rast_metrics, metric_output_csv)
+write_csv(rast_val, raw_val_csv)
