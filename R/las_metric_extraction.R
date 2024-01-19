911b26e829fda46de1967fe55d009642f84af54c
Sean Reilly <sean.reilly66@gmail.com>
2024-01-19 15:28:50 -0800

    Added height thresholds and voxel metrics

--- R/las_metric_extraction.R
+++ R/las_metric_extraction.R
@@ -7,7 +7,7 @@
 # Author: Sean Reilly, sean.reilly66@gmail.com
 #
 # Created: 31 Jan 2022
-# Last commit: 5 Dec 2023
+# Last commit: 19 Jan 2024
 #
 # Status: Complete
 #
@@ -52,7 +52,7 @@ metric_fn <- 'R/las_metric_function.R'
 csv_output <-
   'data/predictor_df/las_plot_metrics_{format(Sys.time(), "%Y%m%d%H%M")}.csv'
 
-n_cluster <- 2
+n_cluster <- 4
 
 # ============================= Point cloud metrics ============================
 
@@ -70,28 +70,70 @@ cl <- makeCluster(n_cluster)
 registerDoParallel(cl)
 
 las_metrics <- foreach(
-  las_i = las_files,
+  file_i = las_files,
   .combine = 'rbind',
   .packages = c('lidR', 'tidyverse', 'raster')
 ) %dopar% {
   
   source('R/las_metric_function.R')
   
-  c <- str_extract(las_i, '(?<=[:punct:]c)[:digit:]+(?=_)')
-  p <- str_extract(las_i, '(?<=_p)[:digit:]+')
-  las_method <- str_extract(las_i, '[:alpha:]+(?=_p[:digit:])')
+  c <- str_extract(file_i, '(?<=[:punct:]c)[:digit:]+(?=_)')
+  p <- str_extract(file_i, '(?<=_p)[:digit:]+')
+  las_method <- str_extract(file_i, '[:alpha:]+(?=_p[:digit:])')
   
-  las_i_metrics <- readLAS(las_i) %>%
-  # las_i_metrics <- readLAS(las_i, filter = '-keep_random_fraction 0.01') %>%  
-    cloud_metrics( ~ las_cld_metrics(z = Z)) %>%
-    as_tibble() %>%
-    add_column(
-      campaign = c,
-      plot = p,
-      method = las_method,
-      .before = 1
-    ) %>%
-    add_column(file = las_i)
+  las_i <- readLAS(file_i)
+  # las_i <- readLAS(file_i, filter = '-keep_random_fraction 0.0001')
+  
+  h_thresh <- c(0.25, 0.5, 0.75, 0.9)
+  
+  pnt_metrics <- foreach(
+    h_i = h_thresh,
+    .combine = 'rbind'
+  ) %do% {
+    
+    pnt_metrics <- las_i %>%
+      filter_poi(Z > h_i) %>%
+      cloud_metrics(~ las_cld_metrics(z = Z)) %>%
+      as_tibble() %>%
+      add_column(
+        campaign = c,
+        plot = p,
+        method = las_method,
+        type = 'pntcld',
+        h_thresh = h_i,
+        vox_dim = NA,
+        .before = 1
+      ) %>%
+      add_column(file = file_i)
+    
+  }
+
+  vox_dim <- c(0.1, 0.25, 0.5, 1, 3.7)
+  
+  vox_metrics <- foreach(
+    vox_i = vox_dim,
+    .combine = 'rbind'
+  ) %do% {
+    
+    vox_metrics <- las_i %>%
+      voxelize_points(res = vox_i) %>%
+      cloud_metrics( ~ las_cld_metrics(z = Z)) %>%
+      as_tibble() %>%
+      add_column(
+        campaign = c,
+        plot = p,
+        method = las_method,
+        type = 'voxel',
+        h_thresh = NA,
+        vox_dim = vox_i,
+        .before = 1
+      ) %>%
+      add_column(file = file_i)
+  
+  }
+  
+  las_metrics <- pnt_metrics %>%
+    add_row(vox_metrics)
   
 }
 
