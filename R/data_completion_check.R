f6c772b92e9c6c0bb34af703b2ef2c0384a7634e
Sean Reilly <sean.reilly66@gmail.com>
2024-03-27 13:22:58 -0700

    Add fusion data checks

--- R/data_completion_check.R
+++ R/data_completion_check.R
@@ -16,6 +16,8 @@ zeb <- read_csv('data/predictor_df/zeb_struct_predictors.csv') %>%
 
 planet <- read_csv('data/predictor_df/planet_spec_predictors.csv')
 
+
+
 tls_missing = anti_join(resp, tls)
 tls_samp = inner_join(resp, tls)
 tls_extra = anti_join(tls,resp)
@@ -39,3 +41,22 @@ shared_samp = inner_join(resp, uas, by = c('campaign', 'plot')) %>%
 site_samp = shared_samp %>%
   group_by(site) %>%
   summarize(n = n())
+
+
+
+
+fusion <- read_csv('data/predictor_df/fusion_struct_predictors.csv') %>%
+  filter(metric_type == 'pntcld',
+         h_thresh == 0.25,
+         pnt_cloud == 'full_fusion')
+
+tls_fusion <- fusion %>%
+  filter(lidar_method == 'tls',
+         !is.na(uas_file))
+
+zeb_fusion <- fusion %>%
+  filter(lidar_method == 'zeb',
+         !is.na(uas_file))
+
+tls_fusion_missing = anti_join(shared_samp, tls_fusion, by = c('campaign', 'plot'))
+zeb_fusion_missing = anti_join(shared_samp, zeb_fusion, by = c('campaign', 'plot'))
